5cd6a7f85d64d965f6a395d6b7e7eb591a7b917b
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2021-06-28 23:13:12 +0300

    Add error in input

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,26 +1,23 @@
 import { useState, useEffect } from "react";
 
-
-import ErrorNameMessage from "../ErrorNameMessage/ErrorNameMessage"
+import ErrorNameMessage from "../ErrorNameMessage/ErrorNameMessage";
 import closeModalImg from "../../svg/close.svg";
 import s from "./Modal.module.css";
 
 function Modal({ goodSingle, onCloseModal }) {
   const [inputName, setInputName] = useState("");
   const [inputNumber, setInputNumber] = useState("");
-  const [errorNameField, setErrorNameField] = useState(
-    "This field in required"
-  );
-  const [errorNumberField, seterrorNumberField] = useState(
-    "This field in required"
-  );
+  const [errorNameField, setErrorNameField] = useState("");
+  const [errorNumberField, seterrorNumberField] = useState("");
   const [isErrorGeneral, setIsErrorGeneral] = useState(false);
 
   useEffect(() => {
     if (!errorNameField & !errorNumberField) {
-      setIsErrorGeneral(false)
-    } else{setIsErrorGeneral(true)}
-  },[errorNameField, errorNumberField])
+      setIsErrorGeneral(false);
+    } else {
+      setIsErrorGeneral(true);
+    }
+  }, [errorNameField, errorNumberField]);
 
   const isOnlyLetters = /^[a-zA-Zа-яА-Я]*$/.test(inputName);
   const isOnlyNumbers = /^[0-9]*$/.test(inputNumber);
@@ -55,10 +52,6 @@ function Modal({ goodSingle, onCloseModal }) {
     }
   };
 
-  // const dateForBackend = ()=>{
-  //     console.log("Name: ", inputName, "Number: ", inputNumber)
-  // }
-
   const hundleOrder = () => {
     if (!errorNameField & !errorNumberField) {
       console.log("Name: ", inputName);
@@ -87,29 +80,28 @@ function Modal({ goodSingle, onCloseModal }) {
             <h3 className={s.cardGood__name}>{goodSingle.name}</h3>
             <div className={s.flexContainer}>
               <h3 className={s.cardGood__price}>{goodSingle.price}</h3>
-              <input
-                className={s.cardGood_inputName}
-                type="text"
-                placeholder="Name"
-                value={inputName}
-                onChange={hundleInputName}
-                onBlur={hundleBlur}
-              ></input>
-
-              {/* <>
-                if(errorNameField){<p>{ errorNameField}</p>}
-              </> */}
-
-              {/* <isErrorGeneral ErrorNameMessage errorNameField={ errorNameField}/> */}
 
               <input
-                className={s.cardGood_inputNumber}
+                className={errorNameField ? s.cardGood_inputError : s.cardGood_inputName}
+                  type="text"
+                  placeholder="Name"
+                  value={inputName}
+                  onChange={hundleInputName}
+                  onBlur={hundleBlur}
+                ></input>
+
+                {errorNameField && <div className={s.cardGood_errorNameField}>{errorNameField}</div>}
+
+              <input
+                className={errorNumberField ? s.cardGood_inputError : s.cardGood_inputNumber}
                 type="tel"
                 placeholder="Number"
                 value={inputNumber}
                 onChange={hundleInputNumber}
                 onBlur={hundleBlur}
               ></input>
+
+              {errorNumberField && <div className={s.cardGood_errorNumberField}>{errorNumberField}</div>}
 
               <button
                 type="submit"
